38f592aea4893fc55b334fd6e2434b44d95be47b
parihariyakunal <parihariyakunal@gmail.com>
2026-08-24 17:21:36 +0530

    Add Main class to read and display user input

---
+++ String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ class Main 
+{
+        public static void main(String[] args) 
+{
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("You entered: " + name);
+        
+       
+}
+}

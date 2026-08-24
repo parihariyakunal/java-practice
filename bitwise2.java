86e1ef359ac77604323f249306ab2db2a62ebf25
parihariyakunal <parihariyakunal@gmail.com>
2026-08-24 17:34:22 +0530

    Refactor bitwise operations with new variable names

---
+++ bitwise2.java
@@ -0,0 +1,15 @@
+class BitShift2 
+{
+public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+
+        int Left = a << b;
+        int Right = a >> b;
+        int Unsignedr = a >>> b;
+
+        System.out.println(left);
+        System.out.println(Right);
+        System.out.println(Unsignedr);
+    }
+}

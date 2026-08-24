fd6cae5b0dfedd2a67536798794f0a1e3e53d8dc
parihariyakunal <parihariyakunal@gmail.com>
2026-08-24 17:31:09 +0530

    Add BitwiseEx1 class for bitwise operations

---
+++ bitwise1.java
@@ -0,0 +1,16 @@
+ class BitwiseEx1
+ {
+ public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 3;
+
+        int andResult = a & b;
+        int orResult = a | b;
+        int xorResult = a ^ b;
+
+        System.out.println(andResult);
+        System.out.println(orResult);
+        System.out.println(xorResult);
+    }
+}

b5b8b7f61faf7a15074d93b0d903b7a6a446a6d8
parihariyakunal <parihariyakunal@gmail.com>
2026-08-24 17:17:02 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) 
+{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        System.out.println("Sum: " + (n1 + n2));
+        System.out.println("Diff: " + (n1 - n2));
+        System.out.println("Prod: " + (n1 * n2));
+        System.out.println("div: " + (n1 / n2));
+        System.out.println("Rem: " + (n1 % n2));
+}
+}

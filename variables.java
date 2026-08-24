13ba1808f29102a8b63f54ce0102dceb7cdfb6b9
parihariyakunal <parihariyakunal@gmail.com>
2026-08-24 17:14:42 +0530

    Add basic arithmetic operations in Main class

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+{
+        int a = 15;
+        int b = 4;
+        int sum = a + b;         
+        int difference = a - b;  
+        int product = a * b;     
+        int divide = a / b;    
+        int remainder = a % b;   
+        System.out.println("sum: " + sum);
+        System.out.println("defference: " + difference);
+        System.out.println("product: " + product);
+        System.out.println("divide: " + divide);
+        System.out.println("remainder: " + remainder);
+    }
+}

9bf0905ba04cff3f77402819f82743e501f87721
parihariyakunal <parihariyakunal@gmail.com>
2026-08-26 14:37:44 +0530

    Implement swapping of two integers using XOR

---
+++ Swapping.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int a = 10, b = 15;
+      a=a^b;
+      b=a^b;
+      a=a^b;
+      System.out.println(a + "         " +b);
+    }
+}

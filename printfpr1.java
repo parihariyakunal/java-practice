e7ccd7325ba9430908da34e495f93d45c74d7a5d
parihariyakunal <parihariyakunal@gmail.com>
2026-08-26 16:31:49 +0530

    Update printfpr1.java

--- printfpr1.java
+++ printfpr1.java
@@ -1,1 +1,13 @@
 
+import java.lang.*;
+public class mf
+{
+    public static void main(String[] args) 
+    { int a = 8;
+      float b =9.98f;
+      System.out.printf("%1$f %2$d", b,a);
+
+        
+    }
+
+}
